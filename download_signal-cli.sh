62845f7fbe4836bda6829a2c4f848b79e7189f07
Thomas Salm <thomas@devtom.de>
2021-12-04 16:27:13 +0100

    setting GITHUB_ENV in download script

--- download_signal-cli.sh
+++ download_signal-cli.sh
@@ -4,10 +4,10 @@ OWNER=AsamK
 REPO=signal-cli
 API_URL=https://api.github.com/repos/${OWNER}/${REPO}/releases/latest
 TAG_NAME=`curl --silent "${API_URL}" | jq -r '.tag_name'`
-export TAG_NAME
+echo "tag_name=${TAG_NAME}" >> $GITHUB_ENV
 echo "Latest version: ${TAG_NAME}"
 ASSET=`curl --silent "${API_URL}" | jq -r '.assets[0].name'`
-export ASSET
+echo "asset=${ASSET}" >> $GITHUB_ENV
 echo "Downloading ${ASSET}"
 ASSET_URL=`curl --silent "${API_URL}" | jq -r '.assets[0].browser_download_url'`
 
